fa0e54a1b4b86128be47c44e2786a21810522067
boardgamesandbars <boardgamesandbars@gmail.com>
2017-02-21 12:31:24 -0500

    Create script.js
    collapses navbar after scrolling

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).on('click','.navbar-collapse',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
